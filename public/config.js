c3786fba771cc8b17c3ebf4979f544e6fa9a0537
root <root@DESKTOP-79Q4VUF>
2024-06-27 14:04:55 -0300

    PublicVersion

---
+++ public/config.js
@@ -0,0 +1,11 @@
+const config = {
+    // a baixo as perguntas e opções pra voce escolher. 
+    nomeDaVitima: 'Coloque o nome da pessoa aqui animalzinho',
+    segundaPergunta: 'Agora nos RESTA SABER!!',
+    option1: 'primeira opção aqui',
+    option2: 'segunda opção aqui',
+    option3: 'Quando??', 
+    image: 'meiaNoite.png', // Se quiser trocar a imagem coloca a imagem dentro da pasta public e colocar o nome dela aqui PATETA
+    numero: '555130303030', // O número deve ser inserido sem o dígito '9' extra.
+    mensagem: 'CLARO%20impossivel%20recusar%20um%20convite%20seu' // Use '%20' para espaços devido à API do WhatsApp.
+};
--- public/script.js
+++ public/script.js
@@ -1,36 +1,51 @@
-const option1 = document.getElementById('option1');
-const option2 = document.getElementById('option2');
-const question1 = document.getElementById('question1');
-const question2 = document.getElementById('question2');
-const option3 = document.getElementById('option3');
-const final = document.getElementById('final');
-const finalImage = document.getElementById('finalImage');
-const finalMessage = document.getElementById('finalMessage');
+document.addEventListener('DOMContentLoaded', () => {
+  const option1 = document.getElementById('option1');
+  const option2 = document.getElementById('option2');
+  const option3 = document.getElementById('option3');
+  const question1 = document.getElementById('question1');
+  const question2 = document.getElementById('question2');
+  const final = document.getElementById('final');
+  const finalImage = document.getElementById('finalImage');
+  const finalMessage = document.getElementById('finalMessage');
+  const nomeDaVitima = document.getElementById('nomeDaVitima');
+  const segundaPergunta = document.getElementById('segundaPergunta')
 
-option1.addEventListener('click', () => {
-  question1.style.display = 'none';
-  question2.style.display = 'block';
-});
+  nomeDaVitima.textContent = config.nomeDaVitima;
+  option1.textContent = config.option1;
+  option2.textContent = config.option2;
+  option3.textContent = config.option3;
+  segundaPergunta.textContent = config.segundaPergunta 
+  finalImage.src = config.image;
 
-option2.addEventListener('mouseover', () => {
-  const x = Math.random() * (window.innerWidth - option2.clientWidth * 2); 
-  const y = Math.random() * (window.innerHeight - option2.clientHeight * 2); 
-  option2.style.left = `${x}px`;
-  option2.style.top = `${y}px`;
-  option2.style.position = 'absolute';
-});
+  option1.addEventListener('click', () => {
+    question1.style.display = 'none';
+    question2.style.display = 'block';
+  });
 
-option3.addEventListener('click', () => {
-  question2.style.display = 'none';
-  final.style.display = 'block';
-  finalImage.style.display = 'block';
-  
-  setTimeout(() => {
-    finalImage.style.display = 'none';
-    finalMessage.style.display = 'block';
-  }, 3000);
-  
-  setTimeout(() => {
-    window.location.href = 'https://wa.me/555183280627?text=CLARO%20andre%2C%20impossivel%20recusar%20um%20convite%20seu';
-  }, 5000);
+  option2.addEventListener('mouseover', () => {
+    const maxX = window.innerWidth - option2.clientWidth;
+    const maxY = window.innerHeight - option2.clientHeight;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    option2.style.left = `${x / 2}px`;
+    option2.style.top = `${y / 2}px`;
+    option2.style.position = 'absolute';
+  });
+
+  option3.addEventListener('click', () => {
+    question2.style.display = 'none';
+    final.style.display = 'block';
+    finalImage.style.display = 'block';
+
+    setTimeout(() => {
+      finalImage.style.display = 'none';
+      finalMessage.style.display = 'block';
+    }, 3000);
+
+    setTimeout(() => {
+      window.location.href = `https://wa.me/${config.numero}?text=${config.mensagem}`;
+    }, 5000);
+  });
 });
